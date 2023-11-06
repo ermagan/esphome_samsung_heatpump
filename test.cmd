1f560a3ec54970ff03c8132d4d3b98d8ce534c9b
Marc Ermagan <marc.ermagan@hotmail.com>
2023-11-06 13:09:31 +0100

    Delete test.cmd

--- test.cmd
+++
@@ -1,1 +0,0 @@
-g++ test/main_test.cpp components/samsung_hp/protocol.cpp components/samsung_hp/nasa.cpp components/samsung_hp/non_nasa.cpp components/samsung_hp/util.cpp -Itest   -o test.exe && test.exe
