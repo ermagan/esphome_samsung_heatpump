4a9eac1b58907f9557d40333d464819ec5d0c71b
Marc Ermagan <marc.ermagan@hotmail.com>
2023-11-06 13:07:07 +0100

    Add files via upload

--- test_non_nasa.cmd
+++ test_non_nasa.cmd
@@ -1,1 +1,1 @@
-g++ test/main_test_non_nasa.cpp components/samsung_ac/protocol.cpp components/samsung_ac/nasa.cpp components/samsung_ac/non_nasa.cpp components/samsung_ac/util.cpp -Itest   -o test.exe && test.exe
+g++ test/main_test_non_nasa.cpp components/samsung_hp/protocol.cpp components/samsung_hp/nasa.cpp components/samsung_hp/non_nasa.cpp components/samsung_hp/util.cpp -Itest   -o test.exe && test.exe
--- test_serial.cmd
+++ test_serial.cmd
@@ -1,1 +1,1 @@
-g++ test/main_serial.cpp components/samsung_ac/nasa.cpp -Itest  -o test.exe && test.exe
+g++ test/main_serial.cpp components/samsung_hp/nasa.cpp -Itest  -o test.exe && test.exe
